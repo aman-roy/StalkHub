f384596ee0cb6f1d41bec7206e86f5271603d939
Aman Roy <royaman8757@gmail.com>
2017-05-14 18:47:06 +0530

    basic_retrive function added in helpers

---
+++ helpers.py
@@ -0,0 +1,49 @@
+# This file has all the functions which will 
+# be used in retriving data from GitHub API.
+
+import urllib2
+import json
+
+# Basic retrival of data from 
+# https://api.github.com/users/user_name
+def basic_retrive(user_name):
+
+	# concatenate user name to create link
+	link = "https://api.github.com/users/" + user_name
+	# empty list for collecting things I need
+	box = []
+
+	# Try to open link and if failed return None
+	try:
+		response = urllib2.urlopen(link)
+		data = json.load(response)
+		if not data:
+			return None
+	except:
+		return None
+
+	# Check if the link opened is user type
+	if not data['type'] == "User":
+		return None
+
+	# Empty dict
+	box_feed = {}
+
+	# Collect everything in box and return box 
+	try:
+		box_feed['login'] = data['login']
+		box_feed['avatar_url'] = data['avatar_url']
+		box_feed['html_url'] = data['html_url']
+		box_feed['name'] = data['name']
+		box_feed['company'] = data['company']
+		box_feed['blog'] = data['blog']
+		box_feed['location'] = data['location']
+		box_feed['bio'] = data['bio']
+		box_feed['public_repos'] = data['public_repos']
+		box_feed['public_gists'] = data['public_gists']
+		box_feed['followers'] = data['followers']
+		box_feed['following'] = data['following']
+		box.append(box_feed)
+		return box
+	except:
+		return None
